b4dcbe664fc53a7dd43ee758f0199b76970749f9
yindaheng98 <yindaheng98@gmail.com>
2024-06-02 23:09:32 -0700

    convert it

--- dataset_convert/convert.sh
+++ dataset_convert/convert.sh
@@ -1,3 +1,23 @@
+# !/bin/bash
+convert_n3dv() {
+    # echo \
+    python dataset_convert/n3dv2imgs.py \
+        --path data/$1 \
+        --exec ./data/ffmpeg \
+        --n_frames $2 >./temp.sh && ./temp.sh && rm ./temp.sh
+    # echo \
+    python convert.py \
+        --colmap_executable ".\data\colmap\COLMAP.bat" \
+        -s "data/$1/frame1" \
+        --single_camera_per_image
+    for ((i = 2; i <= $2; ++i)); do
+        # echo \
+        python convert.py \
+            --colmap_executable ".\data\colmap\COLMAP.bat" \
+            -s "data/$1/frame$i" \
+            --single_camera_per_image \
+            --load_cameras_path data/$1/frame1
+    done
+}
 # for coffee_martini
-python dataset_convert/n3dv2imgs.py --path data/coffee_martini --exec ./data/ffmpeg --n_frames 300 > ./temp.sh && ./temp.sh && rm ./temp.sh
-python convert.py --colmap_executable ".\data\colmap\COLMAP.bat" -s "data/coffee_martini/frame1" --single_camera_per_image
+convert_n3dv coffee_martini 300
