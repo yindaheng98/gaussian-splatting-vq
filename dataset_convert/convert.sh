4030dfabe2956a3c90c65ca2cecf20fcc23fc4f2
yindaheng98 <yindaheng98@gmail.com>
2024-06-02 17:55:31 -0700

    convert n3dv

---
+++ dataset_convert/convert.sh
@@ -0,0 +1,2 @@
+# for coffee_martini
+python dataset_convert/n3dv2imgs.py --path data/coffee_martini --exec ./data/ffmpeg --n_frames 300 > ./temp.sh && ./temp.sh && rm ./temp.sh
---
+++ dataset_convert/n3dv2imgs.py
@@ -0,0 +1,28 @@
+import os
+import re
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--exec", type=str, required=True, help="path to the ffmpeg")
+parser.add_argument("--path", type=str, required=True, help="path to the video folder")
+parser.add_argument("--n_frames", type=int, required=True, help="number of frames")
+parser.add_argument("--maxt", type=int, default=4, help="max num of thread")
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    root = args.path
+    t = args.maxt
+    for entry in os.scandir(root):
+        if not re.match(r"cam[0-9][0-9].mp4", entry.name):
+            continue
+        cam = os.path.splitext(entry.name)[0]
+        imgs_dir = root + "/frame%d/input/" + cam + ".png"
+        for i in range(1, args.n_frames + 1):
+            os.makedirs(root + "/frame%d/input" % i, exist_ok=True)
+        cmd = f"{args.exec} -i {root + '/' + entry.name} {imgs_dir} -y &"
+        print(cmd)
+        t -= 1
+        if t <= 0:
+            print("wait")
+            t = args.maxt
+    print("wait")
