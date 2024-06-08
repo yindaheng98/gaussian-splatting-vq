212fcd6d587ab5648b5cd8e5a0090e7f2268c4e6
yindaheng98 <yindaheng98@gmail.com>
2024-06-07 17:43:33 -0700

    better debug

--- debug/diff_ply.py
+++ debug/diff_ply.py
@@ -1,14 +1,18 @@
 import argparse
 import os
+import torch
 from scene import GaussianModel
+import matplotlib.pyplot as plt
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--src", type=str, required=True, help="The source ply.")
 parser.add_argument("--dst", type=str, required=True, help="The destination ply.")
 
+
 def diff(a, b):
     s = min(a.shape[0], b.shape[0])
     return a[:s, ...].detach() - b[:s, ...].detach()
+
 
 if __name__ == "__main__":
     args = parser.parse_args()
@@ -18,4 +22,8 @@ if __name__ == "__main__":
     gaussians_dst.load_ply(os.path.join(args.dst, "point_cloud.ply"))
     print(gaussians_src, gaussians_dst)
     diff_xyz = diff(gaussians_src.get_xyz, gaussians_dst.get_xyz)
-    print("xyz", diff_xyz.min(), diff_xyz.max(), diff_xyz.abs().mean(axis=0))
+    dist_xyz = torch.norm(diff_xyz, p=2, dim=-1)
+    fig = plt.figure(figsize=(12, 2))
+    ax = fig.subplots()
+    ax.hist(dist_xyz.detach().cpu().numpy(), bins=100)
+    plt.show()
