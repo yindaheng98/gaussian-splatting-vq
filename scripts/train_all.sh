455bcc0ce9b503a0e0671c5b946018552b7a5a96
yindaheng98 <yindaheng98@gmail.com>
2024-06-07 22:46:45 -0700

    only train necessary

--- scripts/train_all.sh
+++ scripts/train_all.sh
@@ -1,19 +1,27 @@
 # !/bin/bash
 train() {
+    if [ ! -e "output/$1/frame1/point_cloud/iteration_$3/point_cloud.ply" ]; then
     # echo \
     python train.py \
         -s data/$1/frame1 \
-        --model_path output/coffee_martini/frame1
+        --model_path output/$1/frame1 \
+        --iterations $3
+    fi
     for ((i = 2; i <= $2; ++i)); do
+        if [ ! -e "output/$1/frame$(expr $i - 1)/point_cloud/iteration_$3/point_cloud.ply" ]; then
+            continue
+        fi
+        if [ ! -e "output/$1/frame$i/point_cloud/iteration_$3/point_cloud.ply" ]; then
         # echo \
         python train.py \
             -s data/$1/frame$i \
             --model_path output/$1/frame$i \
-            --start_ply output/$1/frame$(expr $i - 1)/point_cloud/iteration_30000/point_cloud.ply \
+            --iterations $3 \
+            --start_ply output/$1/frame$(expr $i - 1)/point_cloud/iteration_$3/point_cloud.ply \
             --densify_until_iter 0 \
-            --iterations 7000 \
             --position_lr_init 0.0016
+        fi
     done
 }
 
-train coffee_martini 300
+train coffee_martini 300 30000
