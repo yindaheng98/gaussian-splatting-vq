9da71c919e4c1dcc2087d4f77d94df4e1d0600c2
yindaheng98 <yindaheng98@gmail.com>
2024-06-07 23:19:14 -0700

    show more hist

--- debug/diff_ply.py
+++ debug/diff_ply.py
@@ -14,6 +14,10 @@ def diff(a, b):
     return a[:s, ...].detach() - b[:s, ...].detach()
 
 
+def clamp(p):
+    return torch.clamp(p, max=p.topk(p.shape[0]//100).values[-1])
+
+
 if __name__ == "__main__":
     args = parser.parse_args()
     gaussians_src = GaussianModel(sh_degree=3)
@@ -21,10 +25,23 @@ if __name__ == "__main__":
     gaussians_dst = GaussianModel(sh_degree=3)
     gaussians_dst.load_ply(os.path.join(args.dst, "point_cloud.ply"))
     print(gaussians_src, gaussians_dst)
+
+    fig = plt.figure(figsize=(12, 6))
+    ax = fig.subplots(nrows=3)
+
     diff_xyz = diff(gaussians_src.get_xyz, gaussians_dst.get_xyz)
     dist_xyz = torch.norm(diff_xyz, p=2, dim=-1)
-    dist_xyz_clamp = torch.clamp(dist_xyz, max=dist_xyz.topk(dist_xyz.shape[0]//100).values[-1])
-    fig = plt.figure(figsize=(12, 2))
-    ax = fig.subplots()
-    ax.hist(dist_xyz_clamp.detach().cpu().numpy(), bins=100)
+    dist_xyz_clamp = clamp(dist_xyz)
+    ax[0].hist(dist_xyz_clamp.detach().cpu().numpy(), bins=100)
+
+    diff_rotation = diff(gaussians_src.get_rotation, gaussians_dst.get_rotation)
+    diff_rotation_max = torch.abs(diff_rotation).max(dim=-1).values
+    diff_rotation_clamp = clamp(diff_rotation_max)
+    ax[1].hist(diff_rotation_clamp.detach().cpu().numpy(), bins=100)
+
+    diff_scaling = diff(gaussians_src.get_scaling, gaussians_dst.get_scaling)
+    diff_scaling_max = torch.abs(diff_scaling).max(dim=-1).values
+    diff_scaling_clamp = clamp(diff_scaling_max)
+    ax[2].hist(diff_scaling_clamp.detach().cpu().numpy(), bins=100)
+
     plt.show()
