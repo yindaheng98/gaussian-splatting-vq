7ad2789cb84c62d3c10a344d159974aa39e7c8ca
yindaheng98 <yindaheng98@gmail.com>
2024-06-07 23:33:17 -0700

    show all

--- debug/diff_ply.py
+++ debug/diff_ply.py
@@ -18,6 +18,13 @@ def clamp(p):
     return torch.clamp(p, max=p.topk(p.shape[0]//100).values[-1])
 
 
+def clamp_diff(a, b):
+    d = diff(a, b)
+    d_max = torch.abs(d).max(dim=-1).values
+    d_clamp = clamp(d_max)
+    return d_clamp
+
+
 if __name__ == "__main__":
     args = parser.parse_args()
     gaussians_src = GaussianModel(sh_degree=3)
@@ -26,22 +33,23 @@ if __name__ == "__main__":
     gaussians_dst.load_ply(os.path.join(args.dst, "point_cloud.ply"))
     print(gaussians_src, gaussians_dst)
 
-    fig = plt.figure(figsize=(12, 6))
-    ax = fig.subplots(nrows=3)
+    fig = plt.figure(figsize=(12, 8))
+    ax = fig.subplots(nrows=4)
 
-    diff_xyz = diff(gaussians_src.get_xyz, gaussians_dst.get_xyz)
-    dist_xyz = torch.norm(diff_xyz, p=2, dim=-1)
-    dist_xyz_clamp = clamp(dist_xyz)
-    ax[0].hist(dist_xyz_clamp.detach().cpu().numpy(), bins=100)
+    diff_xyz = clamp_diff(gaussians_src._xyz, gaussians_dst._xyz)
+    ax[0].hist(diff_xyz.detach().cpu().numpy(), bins=100)
+    ax[0].set_ylabel("xyz")
 
-    diff_rotation = diff(gaussians_src.get_rotation, gaussians_dst.get_rotation)
-    diff_rotation_max = torch.abs(diff_rotation).max(dim=-1).values
-    diff_rotation_clamp = clamp(diff_rotation_max)
-    ax[1].hist(diff_rotation_clamp.detach().cpu().numpy(), bins=100)
+    diff_rotation = clamp_diff(gaussians_src._rotation, gaussians_dst._rotation)
+    ax[1].hist(diff_rotation.detach().cpu().numpy(), bins=100)
+    ax[1].set_ylabel("rotation")
 
-    diff_scaling = diff(gaussians_src.get_scaling, gaussians_dst.get_scaling)
-    diff_scaling_max = torch.abs(diff_scaling).max(dim=-1).values
-    diff_scaling_clamp = clamp(diff_scaling_max)
-    ax[2].hist(diff_scaling_clamp.detach().cpu().numpy(), bins=100)
+    diff_scaling = clamp_diff(gaussians_src._scaling, gaussians_dst._scaling)
+    ax[2].hist(diff_scaling.detach().cpu().numpy(), bins=100)
+    ax[2].set_ylabel("scaling")
+
+    diff_features_dc = clamp_diff(gaussians_src._features_dc.squeeze(1), gaussians_dst._features_dc.squeeze(1))
+    ax[3].hist(diff_features_dc.detach().cpu().numpy(), bins=100)
+    ax[3].set_ylabel("features_dc")
 
     plt.show()
