4ba116583bc2f30e93f417369e1236199d9b0190
yindaheng98 <yindaheng98@gmail.com>
2024-06-09 07:00:13 +0000

    use different iter

--- scripts/train_all.sh
+++ scripts/train_all.sh
@@ -7,32 +7,35 @@ train() {
         --model_path output/$1/frame1 \
         --iterations $3
     fi
+    LAST_FRAME="output/$1/frame1/point_cloud/iteration_$3/point_cloud.ply"
     for ((i = 2; i <= $2; ++i)); do
-        if [ ! -e "output/$1/frame$(expr $i - 1)/point_cloud/iteration_$3/point_cloud.ply" ]; then
+        if [ ! -e "$LAST_FRAME" ]; then
             continue
         fi
-        if [ ! -e "output/$1/frame$i/point_cloud/iteration_$3/point_cloud.ply" ]; then
+        THIS_FRAME="output/$1/frame$i/point_cloud/iteration_$3/point_cloud.ply"
+        if [ ! -e "$THIS_FRAME" ]; then
         # echo \
         python train.py \
             -s data/$1/frame$i \
             --model_path output/$1/frame$i \
-            --iterations $3 \
+            --iterations $4 \
             --start_ply output/$1/frame$(expr $i - 1)/point_cloud/iteration_$3/point_cloud.ply \
             --densify_until_iter 0 \
             --position_lr_init 0.0016 \
             --incremental
         fi
+        LAST_FRAME="$THIS_FRAME"
     done
 }
 
-train coffee_martini 300 30000
-train cook_spinach 300 30000
-train cut_roasted_beef 300 30000
-train flame_salmon_1 1200 30000
-train flame_steak 300 30000
-train sear_steak 300 30000
+train coffee_martini 300 30000 7000
+train cook_spinach 300 30000 7000
+train cut_roasted_beef 300 30000 7000
+train flame_salmon_1 1200 30000 7000
+train flame_steak 300 30000 7000
+train sear_steak 300 30000 7000
 
-train discussion 300 30000
-train stepin 300 30000
-train trimming 300 30000
-train vrheadset 300 30000
+train discussion 300 30000 7000
+train stepin 300 30000 7000
+train trimming 300 30000 7000
+train vrheadset 300 30000 7000
