a53b1ed2d370bd23e76397133025a6e1e1d9e9fd
yindaheng98 <yindaheng98@gmail.com>
2024-06-09 18:44:14 -0700

    save all the cameras

--- dataset_convert/save_cameras.sh
+++ dataset_convert/save_cameras.sh
@@ -6,5 +6,16 @@ save_cameras() {
     cp -r data/$1/frame1 data/cameras/$1/frame1
 }
 
+rm -rf data/cameras
+
 save_cameras coffee_martini
 save_cameras cook_spinach
+save_cameras cut_roasted_beef
+save_cameras flame_salmon_1
+save_cameras flame_steak
+save_cameras sear_steak
+
+save_cameras discussion
+save_cameras stepin
+save_cameras trimming
+save_cameras vrheadset
