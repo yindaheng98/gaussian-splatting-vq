46bfc9b7cee241d92ed72c16c0f6aeae82caa0ed
yindaheng98 <yindaheng98@gmail.com>
2024-06-10 21:17:28 -0700

    compare points

--- debug/show_distribution.py
+++ debug/show_distribution.py
@@ -1,19 +1,27 @@
 import argparse
 import os
-import torch
+import numpy as np
 from scene import GaussianModel
 import open3d as o3d
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--src", type=str, required=True, help="The source ply.")
+parser.add_argument("--dst", type=str, required=True, help="The destination ply.")
 
 
 if __name__ == "__main__":
     args = parser.parse_args()
-    gaussians = GaussianModel(sh_degree=3)
-    gaussians.load_ply(os.path.join(args.src, "point_cloud.ply"))
-    
-    pcd = o3d.geometry.PointCloud()
-    pcd.points = o3d.utility.Vector3dVector(gaussians._features_dc.detach().squeeze(1).cpu().numpy())
-    pcd.colors = o3d.utility.Vector3dVector(gaussians._features_dc.detach().squeeze(1).cpu().numpy())
-    o3d.visualization.draw_geometries([pcd])
+    gaussians_src = GaussianModel(sh_degree=3)
+    gaussians_src.load_ply(os.path.join(args.src, "point_cloud.ply"))
+    gaussians_dst = GaussianModel(sh_degree=3)
+    gaussians_dst.load_ply(os.path.join(args.dst, "point_cloud.ply"))
+    data_src = gaussians_src.get_features[:, 0, ...].detach().squeeze(1).cpu().numpy()
+    data_dst = gaussians_dst.get_features[:, 0, ...].detach().squeeze(1).cpu().numpy()
+
+    pcd_src = o3d.geometry.PointCloud()
+    pcd_src.points = o3d.utility.Vector3dVector(data_src)
+    pcd_src.colors = o3d.utility.Vector3dVector(data_src)
+    pcd_dst = o3d.geometry.PointCloud()
+    pcd_dst.points = o3d.utility.Vector3dVector(data_dst)
+    pcd_dst.colors = o3d.utility.Vector3dVector(np.array([[1, 1, 1]]*data_dst.shape[0]))
+    o3d.visualization.draw_geometries([pcd_src, pcd_dst])
