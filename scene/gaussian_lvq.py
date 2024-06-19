fef247de0b0b652fc983adadd07e216234322bcc
yindaheng98 <yindaheng98@gmail.com>
2024-06-19 15:51:32 -0700

    not effective calculate distance

--- scene/gaussian_lvq.py
+++ scene/gaussian_lvq.py
@@ -1,4 +1,23 @@
+import torch
+from tqdm import tqdm
+from typing import List
 from .gaussian_kmeans import KMeansGaussianModel, Attribute
+
+
+def distance(a: torch.Tensor, b: torch.Tensor):
+    ab = torch.cat([a, b], dim=0)
+    center = ab.mean(dim=0, keepdim=True)
+    dist = torch.norm(ab-center, dim=1, p=2).mean()
+    return dist
+
+
+def fill_distances(dists: torch.Tensor, datalist: List[torch.Tensor]):
+    n = len(datalist)
+    pbar = tqdm(desc="Calculate distances", total=dists.shape[0])
+    for i in range(n):
+        for j in range(i+1, n):
+            dists[(n-1+n-i)*i//2+j] = distance(datalist[i], datalist[j])
+            pbar.update(1)
 
 
 class LayeredKMeansGaussianModel(KMeansGaussianModel):
@@ -9,4 +28,6 @@ class LayeredKMeansGaussianModel(KMeansGaussianModel):
         kmeans = getattr(self, super().get_name(attr, i))
         data = self.get_data(attr, i).detach()
         quant = super().quantize(attr, i)
+        dists = torch.zeros((kmeans.shape[0]-1)*kmeans.shape[0]//2, dtype=torch.float32, device='cpu')
+        fill_distances(dists, [data[quant == i, ...] for i in range(kmeans.shape[0])])
         raise NotImplementedError("build_codebook not implemented")
