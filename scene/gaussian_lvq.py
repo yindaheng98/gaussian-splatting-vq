c0f1d17572acbd8abe534a5cbb258ccf49a350af
yindaheng98 <yindaheng98@gmail.com>
2024-06-19 16:31:21 -0700

    better

--- scene/gaussian_lvq.py
+++ scene/gaussian_lvq.py
@@ -5,21 +5,21 @@ from .gaussian_kmeans import KMeansGaussianModel, Attribute
 
 
 def knn(kmeans, n=8, batch=4096):
-    """Calculate the distance between the point and k neighbor points"""
-    pass
-    indxs = torch.zeros(kmeans.shape[0], n, dtype=torch.int32, device="cuda")
+    """Get k neighbor points for each point."""
+    neighbors_idx = torch.zeros(kmeans.shape[0], n, dtype=torch.int32, device="cuda")
     dists = torch.zeros(kmeans.shape[0], n, dtype=torch.float32, device="cuda")
     progress_bar = tqdm.tqdm(range(kmeans.shape[0]), desc="Init KMeans center K nearest")
     for i in range(math.ceil(kmeans.shape[0]/batch)):
         dist = torch.norm(kmeans[i*batch:i*batch+batch, ...].unsqueeze(-2) - kmeans, p=2, dim=-1)
         knn = dist.topk(n + 1, largest=False)
         dists[i*batch:i*batch+batch, ...] = knn.values[:, 1:]
-        indxs[i*batch:i*batch+batch, ...] = knn.indices[:, 1:]
+        neighbors_idx[i*batch:i*batch+batch, ...] = knn.indices[:, 1:]
         progress_bar.update(min(i*batch+batch, kmeans.shape[0])-i*batch)
-    return indxs, dists
+    return neighbors_idx, dists
 
 
-def distance(a: torch.Tensor, b: torch.Tensor):
+def vdistance(a: torch.Tensor, b: torch.Tensor):
+    """Virtual distance for quant tree. Just the avg distance from center to points after merge."""
     ab = torch.cat([a, b], dim=0)
     center = ab.mean(dim=0, keepdim=True)
     dist = torch.norm(ab-center, dim=1, p=2).mean()
@@ -27,12 +27,13 @@ def distance(a: torch.Tensor, b: torch.Tensor):
 
 
 def distance_init(kmeans, neighbors_idx, data, quant):
+    """Calculate the virtual distance between the point and their k neighbor points"""
     dists = torch.zeros(neighbors_idx.shape, dtype=torch.float32, device=data.device)
     for center_idx in tqdm.tqdm(range(kmeans.shape[0]), desc="Init KMeans center distance"):
         center_data = data[quant == center_idx, ...]
         for i, neighbor_idx in enumerate(neighbors_idx[center_idx, ...]):
             neighbor_data = data[quant == neighbor_idx]
-            dists[center_idx, i] = distance(center_data, neighbor_data)
+            dists[center_idx, i] = vdistance(center_data, neighbor_data)
     return dists
 
 
