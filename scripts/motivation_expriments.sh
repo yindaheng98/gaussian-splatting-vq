1ba4415e6540f67ac7072817cd135b2623d0d1d5
yindaheng98 <yindaheng98@gmail.com>
2024-06-20 15:35:17 -0700

    quant - enc - dec - dequant

---
+++ scripts/motivation_expriments.sh
@@ -0,0 +1,45 @@
+# !/bin/bash
+# should run after kmeans_build.sh
+
+quantize() {
+    # quant by kmeans
+    python kmeans.py \
+        --src output/$1/frame$2 \
+        --save output/vq-$1/frame$2 \
+        --iteration $3 \
+        quantize \
+        --dst output/vq-$1/frame$2 \
+        --log2-clusters 16 \
+        --log2-clusters-scaling $4 \
+        --log2-clusters-rotation $5 \
+        --log2-clusters-features_dc $6 \
+        --log2-clusters-features_rest $7 \
+        --log2-clusters-opacity $8
+    # compress by draco
+    ./build/Release/draco_encoder \
+        -i output/vq-$1/frame$2/point_cloud/iteration_$3/point_cloud_vq.ply \
+        -o output/vq-$1/frame$2/point_cloud/iteration_$3/point_cloud_vq.drc \
+        -cl 0 \
+        -qp 16 \
+        -point_cloud \
+        -use_metadata
+    # decompress by draco
+    ./build/Release/draco_decoder \
+        -i output/vq-$1/frame$2/point_cloud/iteration_$3/point_cloud_vq.drc \
+        -o output/vq-$1/frame$2/point_cloud/iteration_$3/point_cloud_vq_ddrc.ply
+    # dequant by kmeans
+    python kmeans.py \
+        --src output/$1/frame$2 \
+        --save output/vq-$1/frame$2 \
+        --iteration $3 \
+        dequantize \
+        --dst output/vq-$1/frame$2 \
+        --log2-clusters 16 \
+        --log2-clusters-scaling $4 \
+        --log2-clusters-rotation $5 \
+        --log2-clusters-features_dc $6 \
+        --log2-clusters-features_rest $7 \
+        --log2-clusters-opacity $8
+
+}
+quantize coffee_martini 1 30000 12 10 6 6 6
