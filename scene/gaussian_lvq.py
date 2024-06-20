6f770f3b2cde0d3cf5616c80ac28d105d92525a5
yindaheng98 <yindaheng98@gmail.com>
2024-06-19 17:20:25 -0700

    better organized

--- scene/gaussian_lvq.py
+++ scene/gaussian_lvq.py
@@ -4,22 +4,32 @@ import math
 from .gaussian_kmeans import KMeansGaussianModel, Attribute
 
 
-def knn(kmeans, n=8, batch=4096):
+def knn_init(kmeans, n=8, batch=4096):
     """Get k neighbor points for each point."""
     neighbors_idx = torch.zeros(kmeans.shape[0], n, dtype=torch.int32, device="cuda")
     progress_bar = tqdm.tqdm(range(kmeans.shape[0]), desc="Init KMeans center K nearest")
+    dists = torch.zeros(kmeans.shape[0], n, dtype=torch.float32, device="cuda")
     for i in range(math.ceil(kmeans.shape[0]/batch)):
         dist = torch.norm(kmeans[i*batch:i*batch+batch, ...].unsqueeze(-2) - kmeans, p=2, dim=-1)
         knn = dist.topk(n + 1, largest=False)
+        dists[i*batch:i*batch+batch, ...] = knn.values[:, 1:]
         neighbors_idx[i*batch:i*batch+batch, ...] = knn.indices[:, 1:]
         progress_bar.update(min(i*batch+batch, kmeans.shape[0])-i*batch)
-    return neighbors_idx
+    return neighbors_idx, dists
 
 
 def marged_center(a: torch.Tensor, b: torch.Tensor):
     ab = torch.cat([a, b], dim=0)
     center = ab.mean(dim=0, keepdim=True)
     return center
+
+
+def clusters_init(kmeans, data, quant):
+    """Split the data into clusters"""
+    clusters = [None]*kmeans.shape[0]
+    for i in tqdm.tqdm(range(kmeans.shape[0]), desc="Init KMeans clusters"):
+        clusters[i] = data[quant == i, ...]
+    return clusters
 
 
 def vdistance(a: torch.Tensor, b: torch.Tensor):
@@ -29,13 +39,13 @@ def vdistance(a: torch.Tensor, b: torch.Tensor):
     return dist
 
 
-def distance_init(kmeans, neighbors_idx, data, quant):
+def distance_init(kmeans, neighbors_idx, clusters):
     """Calculate the virtual distance between the point and their k neighbor points"""
-    dists = torch.zeros(neighbors_idx.shape, dtype=torch.float32, device=data.device)
+    dists = torch.zeros(neighbors_idx.shape, dtype=torch.float32)
     for center_idx in tqdm.tqdm(range(kmeans.shape[0]), desc="Init KMeans center distance"):
-        center_data = data[quant == center_idx, ...]
+        center_data = clusters[center_idx]
         for i, neighbor_idx in enumerate(neighbors_idx[center_idx, ...]):
-            neighbor_data = data[quant == neighbor_idx]
+            neighbor_data = clusters[neighbor_idx]
             dists[center_idx, i] = vdistance(center_data, neighbor_data)
     return dists
 
@@ -45,9 +55,13 @@ class LayeredKMeansGaussianModel(KMeansGaussianModel):
 
     def build_codebook(self, log2_clusters: int, attr: Attribute, i=0):
         super().load_codebook(self.dirpath, log2_clusters, attr, i)
+
         kmeans = getattr(self, super().get_name(attr, i))
+        neighbors_idx, kdists = knn_init(kmeans)
+
         data = self.get_data(attr, i).detach()
         quant = super().quantize(attr, i)
-        neighbors_idx = knn(kmeans)
-        dists = distance_init(kmeans, neighbors_idx, data, quant)
+        clusters = clusters_init(kmeans, data, quant)
+
+        dists = distance_init(kmeans, neighbors_idx, clusters)
         raise NotImplementedError("build_codebook not implemented")
