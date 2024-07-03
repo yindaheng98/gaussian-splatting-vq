7e1453101a26d91275339aaff24451f604aa184f
yindaheng98 <yindaheng98@gmail.com>
2024-07-03 16:12:54 -0700

    try dense it

---
+++ dataset_convert/dense_reconstruction.sh
@@ -0,0 +1,23 @@
+# !/bin/bash
+COLMAP_EXE_PATH=colmap
+dense() {
+    cp data/$1/frame1/sparse/0/* data/$1/frame1/sparse
+    # echo \
+    $COLMAP_EXE_PATH patch_match_stereo \
+        --workspace_path data/$1/frame1 \
+        --workspace_format COLMAP \
+        --PatchMatchStereo.max_image_size 2000 \
+        --PatchMatchStereo.geom_consistency true
+}
+
+dense coffee_martini
+dense cook_spinach
+dense cut_roasted_beef
+dense flame_salmon_1
+dense flame_steak
+dense sear_steak
+
+dense discussion
+dense stepin
+dense trimming
+dense vrheadset
