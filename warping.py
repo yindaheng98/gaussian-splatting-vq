5418ba95886bc61fc0476a71ade70c1f79ad4c74
yindaheng98 <yindaheng98@gmail.com>
2024-07-03 12:07:39 -0700

    no more device

--- warping.py
+++ warping.py
@@ -21,7 +21,7 @@ def read_camera(idx):
 
 
 def read_color(idx):
-    return cv2.imread(idx + ".png")
+    return torch.tensor(cv2.imread(idx + ".png"))
 
 
 def read_camera_color(idx):
@@ -29,7 +29,7 @@ def read_camera_color(idx):
 
 
 def read_depth(idx):
-    return np.load(idx + ".depth.npz")["depth"][0, ...]
+    return torch.tensor(np.load(idx + ".depth.npz")["depth"][0, ...])
 
 
 def read_camera_depth(idx):
@@ -40,16 +40,14 @@ def read_camera_rgbd(idx):
     return *read_camera(idx), read_color(idx), read_depth(idx)
 
 
-def to_pcd(K, R, t, height, width, depth_raw, device=torch.device("cuda")):
-    with device:
-        uv = torch.ones((height, width, 3), dtype=torch.float32)
-        uv[..., 0] = torch.arange(0, width, dtype=torch.float32).unsqueeze(0).expand(height, -1)
-        uv[..., 1] = torch.arange(0, height, dtype=torch.float32).unsqueeze(1).expand(-1, width)
-        depth = torch.from_numpy(depth_raw).to(device)
-        xyz_camera = torch.inverse(K) @ uv.reshape(-1, 3).T * depth.reshape(-1)
-        # xyz_camera = torch.from_numpy(np.asarray(pcd.points, dtype=np.float32)).T*1000
-        xyz_world = torch.inverse(R) @ (xyz_camera - t.unsqueeze(1))
-        return xyz_world.T.reshape(*uv.shape)
+def to_pcd(K, R, t, height, width, depth):
+    uv = torch.ones((height, width, 3), dtype=torch.float32)
+    uv[..., 0] = torch.arange(0, width, dtype=torch.float32).unsqueeze(0).expand(height, -1)
+    uv[..., 1] = torch.arange(0, height, dtype=torch.float32).unsqueeze(1).expand(-1, width)
+    xyz_camera = torch.inverse(K) @ uv.reshape(-1, 3).T * depth.reshape(-1)
+    # xyz_camera = torch.from_numpy(np.asarray(pcd.points, dtype=np.float32)).T*1000
+    xyz_world = torch.inverse(R) @ (xyz_camera - t.unsqueeze(1))
+    return xyz_world.T.reshape(*uv.shape)
 
 
 def projection(K, R, t, height, width, xyz):
@@ -60,8 +58,7 @@ def projection(K, R, t, height, width, xyz):
     return uv
 
 
-device = torch.device("cuda")
-with device:
+with torch.device("cuda"):
     idx_src = "output/coffee_martini/frame1/train_interp/ours_30000/renders/00000"
     xyz = to_pcd(*read_camera_depth(idx_src))
     color_raw = read_color(idx_src)
@@ -69,7 +66,7 @@ with device:
     K, R, t, height, width = read_camera(idx_dst)
     uv = projection(K, R, t, height, width, xyz)
     grid = uv[..., :2] / torch.tensor([[[width, height]]]) - 0.5
-    color = torch.from_numpy(read_color(idx_src)).to(device)
+    color = torch.tensor(read_color(idx_src))
     warped = F.grid_sample(color.permute(2, 0, 1).unsqueeze(0).type(torch.float32), grid.unsqueeze(0),
                            mode='bilinear', align_corners=True)[0, ...].type(torch.uint8)
 
