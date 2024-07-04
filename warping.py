0244005649e23c30cd9ba5a7b8941a93b989a71f
yindaheng98 <yindaheng98@gmail.com>
2024-07-03 20:30:32 -0700

    c2w not w2c!!!

--- warping.py
+++ warping.py
@@ -10,14 +10,14 @@ def read_camera(idx):
         camera = json.load(f)
     height, width = camera["height"], camera["width"]
 
-    R = torch.tensor(camera["rotation"])
-    t = torch.tensor(camera["position"])
+    R_c2w = torch.tensor(camera["rotation"])
+    T_c2w = torch.tensor(camera["position"])
     K = torch.tensor([
         [camera["fx"], 0, camera["width"]/2],
         [0, camera["fy"], camera["height"]/2],
         [0, 0, 1]
     ])
-    return K, R, t, height, width
+    return K, R_c2w, T_c2w, height, width
 
 
 def read_color(idx):
@@ -40,19 +40,19 @@ def read_camera_rgbd(idx):
     return *read_camera(idx), read_color(idx), read_depth(idx)
 
 
-def to_pcd(K, R, t, height, width, depth):
+def to_pcd(K, R_c2w, T_c2w, height, width, depth):
     uv = torch.ones((height, width, 3), dtype=torch.float32)
     uv[..., 0] = torch.arange(0, width, dtype=torch.float32).unsqueeze(0).expand(height, -1)
     uv[..., 1] = torch.arange(0, height, dtype=torch.float32).unsqueeze(1).expand(-1, width)
     xyz_camera = torch.inverse(K) @ uv.reshape(-1, 3).T * depth.reshape(-1)
     # xyz_camera = torch.from_numpy(np.asarray(pcd.points, dtype=np.float32)).T*1000
-    xyz_world = torch.inverse(R) @ (xyz_camera - t.unsqueeze(1))
+    xyz_world = R_c2w @ xyz_camera + T_c2w.unsqueeze(1)
     return xyz_world.T.reshape(*uv.shape)
 
 
-def projection(K, R, t, height, width, xyz):
+def projection(K, R_c2w, T_c2w, height, width, xyz):
     xyz_world = xyz.reshape(-1, 3).T
-    xyz_camera = R @ xyz_world + t.unsqueeze(1)
+    xyz_camera = torch.inverse(R_c2w) @ (xyz_world - T_c2w.unsqueeze(1))
     uvz = K @ xyz_camera
     uv = (uvz/uvz[-1, ...]).T.reshape(height, width, 3)
     return uv
@@ -60,8 +60,9 @@ def projection(K, R, t, height, width, xyz):
 
 def render(uv, color_ref, width, height):
     uv_idx = uv[..., :2].round().type(torch.int)
-    warped = torch.zeros_like(color_ref)
-    warped[uv_idx[..., 1].clamp(0, height-1), uv_idx[..., 0].clamp(0, width-1), ...] = color_ref
+    # warped = torch.zeros_like(color_ref)
+    # warped[uv_idx[..., 1].clamp(0, height-1), uv_idx[..., 0].clamp(0, width-1), ...] = color_ref # inverse
+    warped = color_ref[uv_idx[..., 1].clamp(0, height-1), uv_idx[..., 0].clamp(0, width-1), ...]
     return warped
 
 
@@ -76,15 +77,15 @@ def warp(uv, color_ref, width, height):
 with torch.device("cuda"):
     idx_loc = "output/coffee_martini/frame1-SH1/train_interp/ours_30000/renders/00001"  # local rendered image
     idx_ref = "output/coffee_martini/frame1-SH1/train_interp/ours_30000/renders/00000"  # reference image
-    K, R, t, height, width, depth = read_camera_depth(idx_loc)
-    xyz = to_pcd(K, R, t, height, width, depth)  # xyz[uv on local rendered image] = pos in 3D space
+    K, R_c2w, T_c2w, height, width, depth = read_camera_depth(idx_loc)
+    xyz = to_pcd(K, R_c2w, T_c2w, height, width, depth)  # xyz[uv on local rendered image] = pos in 3D space
     K_r, R_r, t_r, height_r, width_r = read_camera(idx_ref)
     uv = projection(K_r, R_r, t_r, height_r, width_r, xyz)  # uv[uv on local rendered image] = uv on reference
     grid = uv[..., :2] / torch.tensor([[[width, height]]]) * 2 - 1
     color = torch.tensor(read_color(idx_loc))  # local rendered image
     color_ref = torch.tensor(read_color(idx_ref))  # reference image
-    warped = warp(uv, color, width, height)  # wrap it
-    rendered = render(uv, color, width, height)  # wrap it
+    warped = warp(uv, color_ref, width, height)  # wrap it
+    rendered = render(uv, color_ref, width, height)  # wrap it
 
     import open3d as o3d
     pcd = o3d.geometry.PointCloud()
