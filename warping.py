2d6a45797ab6ae8b4512e9391e010ce621f1bdfa
yindaheng98 <yindaheng98@gmail.com>
2024-07-07 11:21:10 -0700

    fix step by step

--- warping.py
+++ warping.py
@@ -108,10 +108,15 @@ def is_occlusion(uv, depth, height, width):
     """Detect whether the pixel is occluded by others when project to another camera"""
 
     # 与其他点有重合的点
-    counts = count(uv, height, width)
-    # counts_back: 对于local rendered image上的每个点，在投影到reference image上后，有多少个点和它重合？
-    counts_back = counts[uv[..., 1], uv[..., 0]]
-    mask_overlap = counts_back > 1
+    counts_onref = count(uv, height, width)
+    # counts_onloc: 对于local rendered image上的每个点，在投影到reference image上后，有多少个点和它重合？
+    counts_onloc = counts_onref[uv[..., 1], uv[..., 0]]
+    mask_overlap = counts_onloc > 1
+
+    # 投影到reference image上各像素处的最小深度
+    mindepth_onref = get_min_depth(uv, depth, height, width)
+    # mindepth_onloc: 对于local rendered image上的每个点，在投影到reference image上后，所有和它重合的点的深度的最小值是多少？
+    mindepth_onloc = mindepth_onref[uv[..., 1], uv[..., 0]]
 
     # 图像边缘的点不算在重合点中
     uv_tmp = uv[mask_overlap, ...]
@@ -119,11 +124,9 @@ def is_occlusion(uv, depth, height, width):
     mask_tmp[torch.logical_or(uv_tmp[..., 0] <= 0, uv_tmp[..., 0] >= width-1)] = False
     mask_tmp[torch.logical_or(uv_tmp[..., 1] <= 0, uv_tmp[..., 1] >= height-1)] = False
     mask_overlap[mask_overlap.clone()] = mask_tmp
-    # 重合点中与深度最低的点深度相差不大的点不算在重合点中
-    min_depth = get_min_depth(uv, depth, height, width)
-    # min_depth_back: 对于local rendered image上的每个点，在投影到reference image上后，所有和它重合的点的深度的最小值是多少？
-    min_depth_back = min_depth[uv[..., 1], uv[..., 0]]
-    depthdiff = torch.abs(depth[mask_overlap] - min_depth_back[mask_overlap])
+
+    # 计算深度差，用于区分重合点中：1、哪些点被其他点遮挡了；2、哪些点遮挡了其他点
+    depthdiff = torch.abs(depth[mask_overlap] - mindepth_onloc[mask_overlap])
     # import matplotlib.pyplot as plt
     # fig = plt.figure(figsize=(16, 12))
     # ax = fig.subplots()
@@ -131,20 +134,20 @@ def is_occlusion(uv, depth, height, width):
     # ax.hist(bins[:-1], bins, weights=counts)
     # plt.show()
 
-    # 哪些点被其他点遮挡了
+    # 判定哪些点被其他点遮挡了：1、重合点；2、和最小深度之差大于阈值
     mask_tmp = mask_overlap[mask_overlap]
     mask_tmp[depthdiff < depth_diff_thr_for_occlusion] = False
     mask_occluded = mask_overlap.clone()
     mask_occluded[mask_overlap] = mask_tmp
 
-    # 哪些点遮挡了其他点
-    occluded_pos_on_ref = uv[mask_occluded, ...]  # 所有在local rendered image上判定为被遮挡的点在reference image上的位置
-    occluded_mask_on_ref = torch.zeros(size=(height, width), dtype=torch.uint8).type(torch.bool)
-    occluded_mask_on_ref[occluded_pos_on_ref[..., 1], occluded_pos_on_ref[..., 0]] = True  # 在reference image上标记上述位置
-    mask_occlude = occluded_mask_on_ref[uv[..., 1], uv[..., 0]]  # 将在reference image上标记的位置再投影回local rendered image上
-    mask_occlude &= ~mask_occluded  # 不是被遮挡的点就是遮挡别人的点
+    # 判定哪些点遮挡了其他点：1、在reference image上和被遮挡点重合；2、未被判定为被遮挡点
+    pos_occluded_onref = uv[mask_occluded, ...]  # 所有在local rendered image上判定为被遮挡的点在reference image上的位置
+    mask_occluded_onref = torch.zeros(size=(height, width), dtype=torch.uint8).type(torch.bool)
+    mask_occluded_onref[pos_occluded_onref[..., 1], pos_occluded_onref[..., 0]] = True  # 在reference image上标记上述位置
+    mask_occlude = mask_occluded_onref[uv[..., 1], uv[..., 0]]  # 将在reference image上标记的位置再投影回local rendered image上
+    mask_occlude &= ~mask_occluded  # 未被判定为被遮挡点
     # mask_tmp = mask_overlap[mask_overlap]
-    # mask_tmp[depthdiff >= depth_diff_thr_for_occlusion] = False
+    # mask_tmp[depthdiff >= depth_diff_thr_for_occlusion] = False  # 有些点的重合不是因为遮挡而是因为在同一个面上收缩导致的，所有这样不行
     # mask_occlude = mask_overlap.clone()
     # mask_occlude[mask_overlap] = mask_tmp
     return mask_occluded, mask_occlude
@@ -248,14 +251,19 @@ def warp(uv, color_ref, depth, height, width):
     # warped[uv_idx[..., 1], uv_idx[..., 0], ...] = color_ref  # inverse
 
     mask_occluded, mask_occlude = is_occlusion(uv_idx, depth, height, width)
+    # warped[mask_occluded, :] = torch.tensor([0, 0, 255], dtype=warped.dtype)  # debug
+    # warped[mask_occlude, :] = torch.tensor([0, 255, 0], dtype=warped.dtype)  # debug
+    # return warped
+
     # mask_occluded_last = mask_occluded.clone()  # debug
     # warped, mask_occluded = error_erosion(warped, mask_occluded, mask_occlude)
     # warped[mask_occluded_last, :] = torch.tensor([255, 0, 0], dtype=warped.dtype)  # debug
     # warped[mask_occluded, :] = torch.tensor([0, 255, 0], dtype=warped.dtype)  # debug
-    while mask_occluded.sum() > 0:
-        warped, mask_occluded = error_erosion(warped, mask_occluded, mask_occlude)
     # warped[mask_occlude, :] = torch.tensor([0, 255, 0], dtype=warped.dtype)  # debug
     # warped[edge, :] = torch.tensor([0, 0, 255], dtype=warped.dtype)
+    # return warped
+    while mask_occluded.sum() > 0:
+        warped, mask_occluded = error_erosion(warped, mask_occluded, mask_occlude)
     return warped
 
 
