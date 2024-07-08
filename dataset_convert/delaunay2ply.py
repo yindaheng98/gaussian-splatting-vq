ccecf5bb0e6baca56e1c8b25d664ba8ec1349f39
yindaheng98 <yindaheng98@gmail.com>
2024-07-07 21:20:43 -0700

    get color from nearby point

--- dataset_convert/delaunay2ply.py
+++ dataset_convert/delaunay2ply.py
@@ -1,13 +1,47 @@
 import argparse
-from plyfile import PlyData, PlyElement
+import torch
+import numpy as np
+from plyfile import PlyData
+from tqdm import tqdm
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--delaunay", type=str, required=True, help="path to the delaunay point cloud")
 parser.add_argument("--reference", type=str, required=True, help="path to the reference point cloud")
+parser.add_argument("--batch", type=int, default=1024)
+
+
+def read_ply(path):
+    plydata = PlyData.read(path)
+    vertices = plydata['vertex']
+    positions = torch.tensor(np.vstack([vertices['x'], vertices['y'], vertices['z']])).T
+    colors = torch.tensor(np.vstack([vertices['red'], vertices['green'], vertices['blue']])).T
+    return positions, colors
+
+
+def read_delaunay(path):
+    plydata = PlyData.read(path)
+    vertices = plydata['vertex']
+    positions = torch.tensor(np.vstack([vertices['x'], vertices['y'], vertices['z']])).T
+    return positions
+
+
+def get_color(pos, pos_reference, color_reference):
+    n_points = pos.shape[0]
+    color = torch.zeros(size=(n_points, color_reference.shape[1]), dtype=color_reference.dtype)
+    pbar = tqdm(desc="Processing points", total=n_points)
+    for i in range(0, n_points, args.batch):
+        step = args.batch if i+args.batch < n_points else (n_points-i)
+        dist = torch.norm(pos[i:i+step, ...].unsqueeze(1) - pos_reference.unsqueeze(0), p=2, dim=2)
+        idx = dist.argmin(dim=1)
+        color[i:i+step] = color_reference[idx, ...]
+        pbar.update(step)
+    return color
+
 
 if __name__ == "__main__":
     args = parser.parse_args()
-    data_delaunay = PlyData.read(args.delaunay)
-    data_reference = PlyData.read(args.reference)
-    print(data_delaunay.elements[0])
-    print(data_reference.elements[0])
+    with torch.device(device="cuda"):
+        pos_delaunay = read_delaunay(args.delaunay)
+        pos_reference, color_reference = read_ply(args.reference)
+        color_delaunay = get_color(pos_delaunay, pos_reference, color_reference)
+        print(color_delaunay)
