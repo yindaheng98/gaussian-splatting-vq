53bf374e9524c28909d64595c4d63c57160ad165
yindaheng98 <yindaheng98@gmail.com>
2024-07-08 06:52:47 +0000

    do if not exists

--- dataset_convert/dense_reconstruction.sh
+++ dataset_convert/dense_reconstruction.sh
@@ -2,6 +2,7 @@
 COLMAP_EXE_PATH=colmap
 dense() {
     cp data/$1/frame1/sparse/0/*.bin data/$1/frame1/sparse
+    if [ ! -e "data/$1/frame1/fused.ply" ]; then
     # echo \
     $COLMAP_EXE_PATH patch_match_stereo \
         --workspace_path data/$1/frame1 \
@@ -14,24 +15,33 @@ dense() {
         --workspace_format COLMAP \
         --input_type photometric \
         --output_path data/$1/frame1/fused.ply
+    fi
+    if [ ! -e "data/$1/frame1/meshed-poisson.ply" ]; then
     # echo \
     $COLMAP_EXE_PATH poisson_mesher \
         --input_path data/$1/frame1/fused.ply \
         --output_path data/$1/frame1/meshed-poisson.ply
+    fi
     # echo \
+    if [ ! -e "data/$1/frame1/meshed-delaunay.ply" ]; then
     $COLMAP_EXE_PATH delaunay_mesher \
         --input_path data/$1/frame1 \
         --output_path data/$1/frame1/meshed-delaunay.ply
+    fi
+    if [ ! -e "data/$1/frame1/colorful-delaunay.ply" ]; then
     # echo \
     python dataset_convert/delaunay2ply.py \
-        --delaunay data/$1/meshed-delaunay.ply \
+        --delaunay data/$1/frame1/meshed-delaunay.ply \
         --reference data/$1/frame1/meshed-poisson.ply \
         --save data/$1/frame1/colorful-delaunay.ply
+    fi
+    if [ ! -e "data/$1/frame1/filtered-poisson.ply" ]; then
     # echo \
     python dataset_convert/poisson_filter.py \
-        --poisson data/$1/meshed-poisson.ply \
+        --poisson data/$1/frame1/meshed-poisson.ply \
         --reference data/$1/frame1/colorful-delaunay.ply \
         --save data/$1/frame1/filtered-poisson.ply
+    fi
     rm data/$1/frame1/sparse/0/points3D.ply
     cp data/$1/frame1/filtered-poisson.ply data/$1/frame1/sparse/0/points3D.ply
 }
