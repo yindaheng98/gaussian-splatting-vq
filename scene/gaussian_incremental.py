409d135aad18ad01fc3f01924aad4999906740fd
yindaheng98 <yindaheng98@gmail.com>
2024-07-10 00:17:30 -0700

    strong force avoid large scaling

--- scene/gaussian_incremental.py
+++ scene/gaussian_incremental.py
@@ -44,8 +44,7 @@ class GaussianModelIncremental(GaussianModel):
 
     def reg(self):
         relative_scaling = self._scaling - self.stretch_shrink_start
-        relative_scaling[relative_scaling < 0] *= 0
-        stretch = torch.sqrt((relative_scaling ** 2).sum(-1) + 1e-20).mean()
+        stretch = (torch.exp(relative_scaling[relative_scaling > 0]) - 1 + 1e-20).mean()
         return stretch
 
     def __init__(self, sh_degree: int):
