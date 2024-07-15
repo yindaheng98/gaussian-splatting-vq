b625905e87fdd096e232c39f78c8ccd49a004c67
yindaheng98 <yindaheng98@gmail.com>
2024-07-15 16:33:32 -0700

    do all

--- scripts/svq_build_all.sh
+++ scripts/svq_build_all.sh
@@ -17,4 +17,22 @@ build() {
         --index $INDEX
     fi
 }
-build coffee_martini 1 30000 12 8 features_dc # debug
+build_all_attr() {
+    build $1 $2 $3 16 6 scaling
+    build $1 $2 $3 16 6 rotation
+    build $1 $2 $3 16 6 features_dc
+    build $1 $2 $3 16 6 features_rest 0
+    # build $1 $2 $3 $4 $5 opacity
+}
+
+build_all_attr coffee_martini 1 30000
+build_all_attr cook_spinach 1 30000
+build_all_attr cut_roasted_beef 1 30000
+build_all_attr flame_salmon_1 1 30000
+build_all_attr flame_steak 1 30000
+build_all_attr sear_steak 1 30000
+
+build_all_attr discussion 1 30000
+build_all_attr stepin 1 30000
+build_all_attr trimming 1 30000
+build_all_attr vrheadset 1 30000
