cbc58fee7d5f33459126ea89ecde557eba258c53
yindaheng98 <yindaheng98@gmail.com>
2024-07-17 16:38:16 -0700

    camera dataset

--- run.py
+++ run.py
@@ -1,44 +1,6 @@
 import argparse
-import numpy as np
-import re
-
-import torch
-# from utils.camera_utils import quaternion_to_matrix # circular import? why?
 from typing import NamedTuple
-
-
-def quaternion_to_matrix(quaternions: torch.Tensor) -> torch.Tensor:
-    # From pytorch3d.transforms.rotation_conversions
-    """
-    Convert rotations given as quaternions to rotation matrices.
-
-    Args:
-        quaternions: quaternions with real part first,
-            as tensor of shape (..., 4).
-
-    Returns:
-        Rotation matrices as tensor of shape (..., 3, 3).
-    """
-    r, i, j, k = torch.unbind(quaternions, -1)
-    # pyre-fixme[58]: `/` is not supported for operand types `float` and `Tensor`.
-    two_s = 2.0 / (quaternions * quaternions).sum(-1)
-
-    o = torch.stack(
-        (
-            1 - two_s * (j * j + k * k),
-            two_s * (i * j - k * r),
-            two_s * (i * k + j * r),
-            two_s * (i * j + k * r),
-            1 - two_s * (i * i + k * k),
-            two_s * (j * k - i * r),
-            two_s * (i * k - j * r),
-            two_s * (j * k + i * r),
-            1 - two_s * (i * i + j * j),
-        ),
-        -1,
-    )
-    return o.reshape(quaternions.shape[:-1] + (3, 3))
-
+from scene.camera_dataset import CameraPoseDataset, Pose
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--viewport", type=str, required=True, help="Path to the camera pose.")
@@ -46,7 +8,28 @@ parser.add_argument("--fovx", type=float, default=888, help="Camera fov x axis."
 parser.add_argument("--fovy", type=float, default=888, help="Camera fov y axis.")
 parser.add_argument("--width", type=float, default=1600, help="Camera width.")
 parser.add_argument("--height", type=float, default=1200, help="Camera height.")
-# TODO: 读取预测视角数据
+parser.add_argument("--buffersize", type=int, default=10, help="Camera buffer size.")
+
+
+class Camera(NamedTuple):
+    pose: Pose
+    fovx: float
+    fovy: float
+    width: int
+    height: int
+
+
+def predict_viewport(pose_history, fovx: float, fovy: float, width: int, height: int):
+    return Camera(pose=pose_history[-1], fovx=fovx, fovy=fovy, width=width, height=height)
+
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    camerapose_dataset = CameraPoseDataset(args.viewport)
+    for camera in camerapose_dataset:
+        print(camera)
+
+
 # TODO: 渲染预测视角处的低清图
 # TODO: 读取带宽数据
 # TODO: 决策量化级别, 预测视角内塞满带宽
@@ -54,62 +37,3 @@ parser.add_argument("--height", type=float, default=1200, help="Camera height.")
 # TODO: 渲染色彩失真图
 # TODO: 色彩恢复
 # TODO: 测质量
-
-
-class ReaderIter:
-    def __init__(self, cameras):
-        self.i = 0
-        self.cameras = cameras
-
-    def __next__(self):
-        if self.i < len(self.cameras):
-            return self.cameras[self.i]
-        raise StopIteration
-
-
-class Pose(NamedTuple):
-    timestamp: float
-    R: torch.Tensor
-    T: torch.Tensor
-
-
-class PoseReader:
-    regex = re.compile(r"^([0-9.]+), \(([-0-9.]+), ([-0-9.]+), ([-0-9.]+)\), \(([-0-9.]+), ([-0-9.]+), ([-0-9.]+), ([-0-9.]+)\)")
-
-    def __init__(self, path):
-        self.path = path
-
-    def __iter__(self):
-        cameras = []
-        with open(self.path, 'r') as f:
-            ts, Ts, quaternions = [], [], []
-            for line in f.readlines():
-                find = re.findall(self.regex, line)[0]
-                find = [float(f) for f in find]
-                t, T, quaternion = find[0], find[1:4], find[4:8]
-                ts.append(t)
-                Ts.append(T)
-                quaternions.append(quaternion)
-            Ts = torch.tensor(Ts)
-            Rs = quaternion_to_matrix(torch.tensor(quaternions))
-            return ReaderIter([Pose(timestamp=t, R=Rs[i, ...], T=Ts[i, ...]) for i, t in enumerate(ts)])
-
-
-class Camera(NamedTuple):
-    P: Pose
-    FovY: np.array
-    FovX: np.array
-    image: np.array
-    width: int
-    height: int
-
-
-def predict_viewport(history):
-    return history[-1]
-
-
-if __name__ == "__main__":
-    args = parser.parse_args()
-    camera_reader = PoseReader(args.viewport)
-    for camera in camera_reader:
-        print(camera)
---
+++ scene/camera_dataset.py
@@ -0,0 +1,47 @@
+import re
+import torch
+from torch.utils.data import Dataset
+from typing import NamedTuple
+from utils.camera_utils import quaternion_to_matrix
+
+
+class Pose(NamedTuple):
+    timestamp: float
+    R: torch.Tensor
+    T: torch.Tensor
+
+
+class CameraPoseDataset(Dataset):
+    regex = re.compile(r"^([0-9.]+), \(([-0-9.]+), ([-0-9.]+), ([-0-9.]+)\), \(([-0-9.]+), ([-0-9.]+), ([-0-9.]+), ([-0-9.]+)\)")
+
+    def __init__(self, path, history_size=8, prediction_stride=4):
+        self.history_size = history_size
+        self.prediction_stride = prediction_stride
+        with open(path, 'r') as f:
+            ts, Ts, quaternions = [], [], []
+            for line in f.readlines():
+                find = re.findall(self.regex, line)[0]
+                find = [float(f) for f in find]
+                t, T, quaternion = find[0], find[1:4], find[4:8]
+                ts.append(t)
+                Ts.append(T)
+                quaternions.append(quaternion)
+            Ts = torch.tensor(Ts)
+            Rs = quaternion_to_matrix(torch.tensor(quaternions))
+            self.poses = [Pose(timestamp=t, R=Rs[i, ...], T=Ts[i, ...]) for i, t in enumerate(ts)]
+
+    def __len__(self):
+        return len(self.poses) - self.history_size - self.prediction_stride
+
+    def __getitem__(self, idx):
+        history = self.poses[idx:idx+self.history_size]
+        history_timestamp = torch.tensor([h.timestamp for h in history])
+        history_R = torch.stack([h.R for h in history], dim=0)
+        history_T = torch.stack([h.T for h in history], dim=0)
+        history_ = {"timestamp": history_timestamp, "R": history_R, "T": history_T}
+        groundtruth = self.poses[idx+self.history_size+self.prediction_stride]
+        groundtruth_timestamp = groundtruth.timestamp
+        groundtruth_R = groundtruth.R
+        groundtruth_T = groundtruth.T
+        groundtruth_ = {"timestamp": groundtruth_timestamp, "R": groundtruth_R, "T": groundtruth_T}
+        return history_, groundtruth_
