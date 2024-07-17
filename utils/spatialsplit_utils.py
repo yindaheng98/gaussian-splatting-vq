3b2d41cec25c6b3dcf7faad6d587d6e4f2791d8d
yindaheng98 <yindaheng98@gmail.com>
2024-07-16 21:29:13 -0700

    scale to 01

--- utils/spatialsplit_utils.py
+++ utils/spatialsplit_utils.py
@@ -7,7 +7,7 @@ def spatial_split(xyz: torch.Tensor, xyz_tile_size):
     floors = (xyz/tile_size.unsqueeze(0)).floor()
     blkids = floors.type(torch.int32)
     print("spatial_split", xyz_tile_size, "blks", blkids.unique(dim=0).shape[0], blkids.max(dim=0).values-blkids.min(dim=0).values)
-    rests = xyz - floors*tile_size
+    rests = xyz/tile_size - floors
     return blkids, rests
 
 
@@ -30,7 +30,7 @@ def spatial_merge(blkids: torch.Tensor, rests: torch.Tensor, xyz_tile_size):
     print("spatial_merge", xyz_tile_size, "blks", "blks", blkids.max(dim=0).values-blkids.min(dim=0).values)
     tile_size = torch.Tensor(xyz_tile_size[:3]).to(rests.device)
     floors = blkids.type(torch.float32)
-    data = floors*tile_size.unsqueeze(0)+rests
+    data = (floors+rests)*tile_size.unsqueeze(0)
     return data
 
 
