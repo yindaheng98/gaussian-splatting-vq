cdd560c93b47612b107fdb3994f3923a52be22d9
yindaheng98 <yindaheng98@gmail.com>
2024-07-18 20:30:04 -0700

    inverse xz

--- scene/camera_dataset.py
+++ scene/camera_dataset.py
@@ -27,9 +27,10 @@ class CameraPoseDataset(Dataset):
                 Ts.append(T)
                 quaternions.append(quaternion)
             Ts = torch.tensor(Ts)
-            Ts[..., 0] *= -1  # for Unity data
-            Ts[..., 2] *= -1  # for Unity data
-            Rs = quaternion_to_matrix(torch.tensor(quaternions)[..., [3, 0, 1, 2]])  # for Unity data
+            Ts[..., 0] *= -1  # for Unity data (inverse x)
+            Ts[..., 2] *= -1  # for Unity data (inverse z)
+            Rs = quaternion_to_matrix(torch.tensor(quaternions)[..., [3, 0, 1, 2]])  # for Unity data (real first)
+            Rs[:, [[False, True, False], [True, False, True], [False, True, False]]] *= -1  # for Unity data (inverse xz)
             # Rs = quaternion_to_matrix(torch.tensor(quaternions))
             self.poses = [Pose(timestamp=t, R=Rs[i, ...], T=Ts[i, ...]) for i, t in enumerate(ts)]
 
