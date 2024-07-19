fc63f8bea29b5dbc70a4a8bb6f8cb31113e28e68
yindaheng98 <yindaheng98@gmail.com>
2024-07-18 17:20:17 -0700

    unity pos

--- scene/camera_dataset.py
+++ scene/camera_dataset.py
@@ -27,6 +27,7 @@ class CameraPoseDataset(Dataset):
                 Ts.append(T)
                 quaternions.append(quaternion)
             Ts = torch.tensor(Ts)
+            Ts[..., 0] *= -1  # for Unity data
             Rs = quaternion_to_matrix(torch.tensor(quaternions))
             self.poses = [Pose(timestamp=t, R=Rs[i, ...], T=Ts[i, ...]) for i, t in enumerate(ts)]
 
