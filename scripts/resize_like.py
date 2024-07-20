85e53316e4238c727cd986bd7163485137b84c44
yindaheng98 <yindaheng98@gmail.com>
2024-07-20 15:32:26 -0700

    resize like

---
+++ scripts/resize_like.py
@@ -0,0 +1,24 @@
+import cv2
+import numpy as np
+import json
+import torch
+import torch.nn.functional as F
+import argparse
+import os
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--read", type=str, required=True, help="Read which image.")
+parser.add_argument("--like", type=str, required=True, help="Like which image.")
+parser.add_argument("--save", type=str, required=True, help="Save to where.")
+parser.add_argument("--scale", type=float, required=True)
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    read = cv2.imread(args.read)
+    downsample = cv2.resize(read, (0, 0), fx=args.scale, fy=args.scale)
+    like = cv2.imread(args.like)
+    h, w, c = like.shape
+    save = cv2.resize(downsample, (w, h))
+    os.makedirs(os.path.dirname(args.save), exist_ok=True)
+    cv2.imwrite(args.save, save)
