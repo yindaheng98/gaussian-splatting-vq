3b109a40a0768e018ea16db32610b2431f6d44ae
yindaheng98 <yindaheng98@gmail.com>
2024-07-20 16:50:57 -0700

    render

--- scripts/prune_expriments.sh
+++ scripts/prune_expriments.sh
@@ -1,14 +1,21 @@
 # !/bin/bash
 prune() {
-    rm -rf output/prune-$1/frame$2/point_cloud/iteration_$3
-    mkdir -p output/prune-$1/frame$2/point_cloud/iteration_$3
-    rm output/prune-$1/frame$2/cfg_args
-    cp output/$1/frame$2/cfg_args output/prune-$1/frame$2/cfg_args
-    rm output/prune-$1/frame$2/point_cloud/iteration_$3/point_cloud.ply
+    rm -rf output/prune-$1/frame$2-$4/point_cloud/iteration_$3
+    mkdir -p output/prune-$1/frame$2-$4/point_cloud/iteration_$3
+    rm output/prune-$1/frame$2-$4/cfg_args
+    cp output/$1/frame$2/cfg_args output/prune-$1/frame$2-$4/cfg_args
+    rm output/prune-$1/frame$2-$4/point_cloud/iteration_$3/point_cloud.ply
     python prune.py \
         --read output/$1/frame$2/point_cloud/iteration_$3/point_cloud.ply \
         --sh-degree 2 \
-        --save output/prune-$1/frame$2/point_cloud/iteration_$3/point_cloud.ply \
+        --save output/prune-$1/frame$2-$4/point_cloud/iteration_$3/point_cloud.ply \
         --target $4
 }
 # prune coffee_martini 1 30000 100000
+render() {
+    rm -rf output/prune-$1/frame$2-$4/train_interp/ours_$3
+    python render.py \
+        -m output/prune-$1/frame$2-$4 \
+        --iteration $3
+}
+# render coffee_martini 1 30000 100000
