099d24bdb115229064a2d7a633c4b749e5da89ba
yindaheng98 <yindaheng98@gmail.com>
2024-07-20 16:38:07 -0700

    prune it

--- scripts/warping_expriments.sh
+++ scripts/warping_expriments.sh
@@ -34,6 +34,19 @@ quantize() {
 
 }
 # quantize coffee_martini 1 30000 12 10 6 6 6
+prune() {
+    rm -rf output/prune-$1/frame$2/point_cloud/iteration_$3
+    mkdir -p output/prune-$1/frame$2/point_cloud/iteration_$3
+    rm output/prune-$1/frame$2/cfg_args
+    cp output/$1/frame$2/cfg_args output/prune-$1/frame$2/cfg_args
+    rm output/prune-$1/frame$2/point_cloud/iteration_$3/point_cloud.ply
+    python prune.py \
+        --read output/$1/frame$2/point_cloud/iteration_$3/point_cloud.ply \
+        --sh-degree 2 \
+        --save output/prune-$1/frame$2/point_cloud/iteration_$3/point_cloud.ply \
+        --target $4
+}
+# prune coffee_martini 1 30000 100000
 render_gt() {
     rm -rf output/$1/frame$2/train_interp/ours_$3
     python render.py \
