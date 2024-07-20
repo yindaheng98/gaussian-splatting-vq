b15590fcee6d029f4ef384d15ace2e259269c32c
yindaheng98 <yindaheng98@gmail.com>
2024-07-19 20:42:24 -0700

    download dataset

--- scripts/download.sh
+++ scripts/download.sh
@@ -2,6 +2,7 @@
 # 下载首帧训练结果
 
 download() {
+    rclone -v --config rclone.conf copy server:gaussian-splatting-vq/data/$1/frame1 ./data/$1/frame1
     rclone -v --config rclone.conf copy server:gaussian-splatting-vq/output/$1/frame1 ./output/$1/frame1
     rclone -v --config rclone.conf copy server:gaussian-splatting-vq/output/vq-$1/frame1 ./output/vq-$1/frame1
 }
