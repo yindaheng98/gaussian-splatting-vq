67e2a799787e85de4c92022ed66cd8fffc45baa3
yindaheng98 <yindaheng98@gmail.com>
2024-07-21 16:53:11 -0700

    black edge

--- warping.py
+++ warping.py
@@ -259,6 +259,10 @@ def warp(uv, color_ref, depth):
     #                        mode='bilinear', align_corners=True)[0, ...].type(torch.uint8).permute(1, 2, 0)
     uv_idx = uv[..., :2]
     uv_idx = uv_idx.round().type(torch.int64)
+    # is_edge = uv_idx[..., 1] < 0
+    # is_edge |= uv_idx[..., 1] >= height
+    # is_edge |= uv_idx[..., 0] < 0
+    # is_edge |= uv_idx[..., 1] >= width
     uv_idx[..., 1].clamp_(0, height-1)
     uv_idx[..., 0].clamp_(0, width-1)
     warped = color_ref[uv_idx[..., 1], uv_idx[..., 0], ...]
@@ -299,6 +303,7 @@ def warp(uv, color_ref, depth):
     # warped[mask_occluded_last, :] = torch.tensor([255, 0, 0], dtype=warped.dtype)  # debug
     # warped[mask_occluded, :] = torch.tensor([0, 255, 0], dtype=warped.dtype)  # debug
     # warped[mask_occlude, :] = torch.tensor([0, 0, 255], dtype=warped.dtype)  # debug
+    # warped[is_edge, ...] = 0
     return warped
 
 
