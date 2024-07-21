89cdf9b94b431e8b0ae16e386f7597f7b3206629
yindaheng98 <yindaheng98@gmail.com>
2024-07-20 20:46:38 -0700

    adjust all params

--- scripts/warping_expriments.sh
+++ scripts/warping_expriments.sh
@@ -119,12 +119,13 @@ convert() {
 }
 # convert coffee_martini 1 30000 12 10 6 6 6
 data4sr() {
-    render_gt $1 "--skip_train --render_train_interp"
-    render_ref $1 $2 "--skip_train --render_train_interp"
+    render_gt $1
+    render_ref $1 $2 $3
+    warping_params=$4
     quant_convert() {
         quantize $1 $2
-        render_vq $1 "--skip_train --render_train_interp"
-        warping $1 "([0-9]+)[.]png" 00000
+        render_vq $1
+        warping $1 "([0-9]+)[.]png" $warping_params
         convert $1 $2
     }
     quant_convert "$1" "8 4 4 4 4"     # worst
@@ -133,17 +134,17 @@ data4sr() {
     quant_convert "$1" "14 13 13 13 4"
     quant_convert "$1" "10 7 7 7 4"
 }
-# data4sr "coffee_martini 1 30000" 900
-# data4sr "cook_spinach 1 30000" 900
-# data4sr "cut_roasted_beef 1 30000" 900
-# data4sr "flame_salmon_1 1 30000" 900
-# data4sr "flame_steak 1 30000" 900
-# data4sr "sear_steak 1 30000" 900
+data4sr "coffee_martini 1 30000" 00000 "2.5 2.0 600"
+data4sr "cook_spinach 1 30000" 00000 "2.5 2.0 600"
+data4sr "cut_roasted_beef 1 30000" 00000 "2.5 2.0 600"
+data4sr "flame_salmon_1 1 30000" 00000 "2.5 2.0 600"
+data4sr "flame_steak 1 30000" 00000 "2.5 2.0 600"
+data4sr "sear_steak 1 30000" 00000 "2.5 2.0 600"
 
-# data4sr "discussion 1 30000" 720
-# data4sr "stepin 1 30000" 720
-# data4sr "trimming 1 30000" 720
-# data4sr "vrheadset 1 30000" 720
+data4sr "discussion 1 30000" 00000 "2.2 1.9 900"
+data4sr "stepin 1 30000" 00000 "2.2 1.9 900"
+data4sr "trimming 1 30000" 00000 "2.2 1.9 900"
+data4sr "vrheadset 1 30000" 00000 "2.2 1.9 900"
 
-# data4sr "taekwondo 1 30000" 810
-# data4sr "walking 1 30000" 810
+data4sr "taekwondo 1 30000" 00002 "1.6 1.3 900"
+data4sr "walking 1 30000" 00002 "2.2 1.3 900"
