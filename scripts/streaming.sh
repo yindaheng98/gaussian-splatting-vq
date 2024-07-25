12ec4762e4b0977eb568f1b8ebdc55d6468973aa
yindaheng98 <yindaheng98@gmail.com>
2024-07-24 20:42:44 -0700

    new names

--- scripts/streaming.sh
+++ scripts/streaming.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 train_prediction() {
-    mkdir -p output/prediction
+    mkdir -p output/run-$1
     python prediction_train.py \
-        --cameras saved_data/$1.txt \
-        --save output/prediction/$1.pth
+        --cameras saved_data/$1-$2.txt \
+        --save output/run-$1/prediction-$2.pth
 }
-# train_prediction coffee_martini
+train_prediction coffee_martini 0
 test_prediction() {
-    mkdir -p output/prediction
+    mkdir -p output/run-$1
     python prediction_test.py \
-        --cameras saved_data/$1.txt \
-        --save output/prediction/$1.pth
+        --cameras saved_data/$1-$2.txt \
+        --save output/run-$1/prediction-$2.pth
 }
-# test_prediction coffee_martini
+test_prediction coffee_martini 0
+
